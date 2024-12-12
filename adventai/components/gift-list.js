ca337ef468459df118f6f6bde0ec5eb655432ae4
Daniele <208292+iamdaniele@users.noreply.github.com>
2024-12-11 18:04:29 -0800

    fix: 404 css

--- adventai/components/gift-list.js
+++ adventai/components/gift-list.js
@@ -1,7 +1,14 @@
 import Domo, {html} from '/app/domo.js';
 
 export class GiftList extends Domo {
-  render() {
+  async getInitialState() {
+    const response = await fetch('/api/gifts');
+    const gifts = await response.json();
+    console.log(gifts) 
+  }
+  
+  async render() {
+    console.log('render')
     if (window.gifts.length === 0) {
       this.style.display = 'none';
       return null;
